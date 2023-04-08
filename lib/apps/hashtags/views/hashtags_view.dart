a4396138a4315c10b06a081cf72d82437b06aab7
adgojoe <jmaulana@adgorithmics.com>
2023-04-08 12:29:19 +0700

    add data count

--- lib/apps/hashtags/views/hashtags_view.dart
+++ lib/apps/hashtags/views/hashtags_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:isar/isar.dart';
 import 'package:tagar/data/data_controller.dart';
 import 'package:tagar/core/widgets/lists.dart';
 import 'package:tagar/data/models/data.dart';
@@ -23,7 +24,8 @@ class HashtagsView extends StatelessWidget {
               Tag data = controller.tags[i];
               return ListTile(
                 dense: true,
-                title: VText('${data.name} (${data.datas.length})'),
+                title: VText(data.name),
+                subtitle: TagDataItem(data.datas),
                 // trailing: IconButton(
                 //   icon: const Icon(Icons.delete),
                 //   onPressed: () => controller.deleteTag(data),
@@ -38,5 +40,41 @@ class HashtagsView extends StatelessWidget {
         },
       ),
     );
+  }
+}
+
+class TagDataItem extends StatefulWidget {
+  final IsarLinks data;
+  const TagDataItem(this.data, {Key? key}) : super(key: key);
+
+  @override
+  State<TagDataItem> createState() => _TagDataItemState();
+}
+
+class _TagDataItemState extends State<TagDataItem> {
+  List<Data> data = [];
+
+  @override
+  void didUpdateWidget(covariant oldWidget) {
+    widget.data.load(overrideChanges: false).then((value) {
+      setState(() {});
+    });
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
+
+  init() async {
+    await widget.data.load();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return VText('${widget.data.length} data');
   }
 }
